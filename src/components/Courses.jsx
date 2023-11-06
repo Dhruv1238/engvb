a57bf31a7503368e4153d2706097a5268114bd38
Dhruv1238 <dhruv4075@gmail.com>
2023-11-06 19:34:46 +0530

    feat: Course Updated

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -16,7 +16,7 @@ const AnimatedCard = ({ children }) => {
 
     return (
         <div ref={ref} >
-            <Card className="feature-card h-[40vh] w-full hover:scale-105 duration-300 flex justify-center gap-3 items-start p-10" ref={cardRef}>
+            <Card className="feature-card md:h-[40vh] min-h-[40vh] w-full hover:scale-105 duration-300 flex justify-center gap-3 items-start p-9 pr-0" ref={cardRef}>
                 {children}
             </Card>
         </div>
@@ -49,15 +49,15 @@ const SystemsList = ({ v1, v2, v3, v4, v5 }) => {
 
 const handleMEPPdf = () => {
     const link = document.createElement("a");
-    link.href = "/MEP.pdf"; // path to your PDF file
-    link.download = "MEP Design.pdf"; // name for downloaded file
+    link.href = "/MEP.pdf";
+    link.download = "MEP Design Syllabus - ENG VB Learning.pdf";
     link.click();
 };
 
 const handleRevitMEPPdf = () => {
     const link = document.createElement("a");
-    link.href = "/MEP.pdf"; // path to your PDF file
-    link.download = "MEP Design.pdf"; // name for downloaded file
+    link.href = "/RevitMEP.pdf";
+    link.download = "Revit MEP Syllabus - ENG VB Learning.pdf";
     link.click();
 };
 
@@ -65,17 +65,20 @@ export const Courses = () => {
 
     return (
         <>
-            <Typography color="white" variant="h1" className='text-center font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full py-12'>
-                We Offer
+            <Typography color="white" variant="h1" className='text-center font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full pt-12'>
+                Courses Offered
+            </Typography>
+            <Typography className="text-gradient font-bold text-md md:text-xl text-center wobble animate-wobble p-10">
+                Click the Courses to get the Detailed Syllabus!!
             </Typography>
             <div id='numerics' className='px-6 md:px-16  grid grid-cols-1 gap-5 sm:grid-cols-2 pb-24'>
                 <div onClick={handleRevitMEPPdf} className=" cursor-pointer">
-                <AnimatedCard>
-                    <Typography color="white" className=" text-gradient font-bold text-4xl md:text-5xl py-2">
-                        REVIT MEP
-                    </Typography>
-                    <SystemsList v1="Electrical Systems" v2="Plumbing and Drainage Systems" v3="Fire Protection Systems" v4="HVAC System" v5="FA, PA, CCTV, and Security Systems" />
-                </AnimatedCard>
+                    <AnimatedCard>
+                        <Typography color="white" className=" text-gradient font-bold text-4xl md:text-5xl py-2">
+                            REVIT MEP
+                        </Typography>
+                        <SystemsList v1="Introduction to BIM and Autodesk Revit" v2="Sketching and Modifying Tools" v3="Modeling of MEPF Systems" v4="Quantification, Detailing, and Scheduling" v5="Tests,&nbsp;Certification,&nbsp;much&nbsp;more" />
+                    </AnimatedCard>
                 </div>
                 <div onClick={handleMEPPdf} className=" cursor-pointer">
                     <AnimatedCard>
@@ -89,11 +92,13 @@ export const Courses = () => {
                     <Typography color="white" className=" text-gradient font-bold text-4xl md:text-5xl py-2">
                         REVIT Structure
                     </Typography>
+                    <SystemsList v1="Introduction to BIM and Autodesk Revit" v2="Sketching and Modifying Tools" v3="Starting and Modeling Structural Projects" v4="Quantification, Detailing, and Scheduling" v5="Tests,&nbsp;Certification,&nbsp;much&nbsp;more" />
                 </AnimatedCard>
                 <AnimatedCard>
-                <Typography color="white" className=" text-gradient font-bold text-4xl md:text-5xl py-2">
+                    <Typography color="white" className=" text-gradient font-bold text-4xl md:text-5xl py-2">
                         REVIT Architecture
                     </Typography>
+                    <SystemsList v1="Introduction to BIM and Autodesk Revit" v2="Sketching and Modifying Tools" v3="Modeling Architecture Projects" v4="Quantification, Detailing, and Scheduling" v5="Massing Tools, Tests, Certification, and Personality" />
                 </AnimatedCard>
                 <AnimatedCard>
                     <Typography color="white" variant="h1">
